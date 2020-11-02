2bb6acb354c1c90daf86a140d46e33887ce5ca16
Daniel Koefoed <dc.koefoed@gmail.com>
2020-11-02 17:48:28 +0100

    pushed up previous changes

--- js/Interface.js
+++ js/Interface.js
@@ -5,18 +5,20 @@ class Interface{
 
     createHeader(lang = "SV"){
         //document.getElementById("rubrik").innerHTML = "";
-        let rubrik = new TextRunner("Portfolio - Daniel Koefoed", "rubrik");
-        rubrik.runner();
+        let title = new TextRunner("Portfolio - Daniel Koefoed", "title");
+        title.runner();
     }
 
     createMenu(){
-        let swe = "test Svenska";
-        let eng = "test Engelska";
-        let text = "";
-        if (lang == "SV"){
-            text = swe;
-        }else if (lang == "EN"){
-            text = eng;
-        }
+        // let swe = "test Svenska";
+        // let eng = "test Engelska";
+        // let text = "";
+        // if (lang == "SV"){
+        //     text = swe;
+        // }else if (lang == "EN"){
+        //     text = eng;
+        // }
+        let innerContent = document.getElementById("inner_content");
+
     }
 }
