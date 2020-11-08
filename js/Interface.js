2302b0520730725682e663be00a9c14583095b5e
Daniel Koefoed <dc.koefoed@gmail.com>
2020-11-08 13:34:13 +0100

    continued work on desktop layout, moved button-functions to Interface-class

--- js/Interface.js
+++ js/Interface.js
@@ -1,10 +1,89 @@
 class Interface{
     constructor(){
+        this.setOn = 0;
+        this.menuOn = 0;
     }
 
     createHeader(){
         let title = new TextRunner("Portfolio ─ Daniel Koefoed", "title");
         title.runner();
+    }
+
+    creatSettingsMenu(){
+        let underTitle = document.getElementById("under_title");
+        let setDrop = document.getElementById("set_drop");
+
+        document.getElementById("settings_btn").addEventListener("click", () =>{
+            if (this.setOn === 0){
+                setDrop.style = "display: flex; flex-direction: column; justify-content: space-between;"
+            }
+            else{
+                setDrop.style = "display:none;"
+            }
+
+            this.setOn = 1 - this.setOn;
+
+            document.getElementById("menu_drop").style = "display:none"
+            this.menuOn = 0;
+        });
+
+        document.getElementById("lang_select_sv").addEventListener("click", () =>{
+            let translator = new Translator("sv");
+            translator.translate();
+
+            let text = "";
+            if (underTitle.innerHTML === "└── " + translations[4].en || underTitle.innerHTML === "└── " + translations[4].sv){
+                text = "└── " + translations[4].sv;
+            }
+            if (underTitle.innerHTML === "└── " + translations[5].en || underTitle.innerHTML === "└── " + translations[5].sv){
+                text = "└── " + translations[5].sv;
+            }
+            if (underTitle.innerHTML === "└── " + translations[6].en || underTitle.innerHTML === "└── " + translations[6].sv){
+                text = "└── " + translations[6].sv;
+            }
+            underTitle.innerHTML = "";
+            this.mobMenuHeader(text);
+
+            setDrop.style = "display:none;"
+            this.setOn = 1 - this.setOn;
+        })
+        document.getElementById("lang_select_en").addEventListener("click", () =>{
+            let translator = new Translator("en");
+            translator.translate();
+
+            let text = "";
+            if (underTitle.innerHTML === "└── " + translations[4].sv || underTitle.innerHTML === "└── " + translations[4].en){
+                text = "└── " + translations[4].en;
+            }
+            if (underTitle.innerHTML === "└── " + translations[5].sv || underTitle.innerHTML === "└── " + translations[5].en){
+                text = "└── " + translations[5].en;
+            }
+            if (underTitle.innerHTML === "└── " + translations[6].sv || underTitle.innerHTML === "└── " + translations[6].en){
+                text = "└── " + translations[6].en;
+            }
+            underTitle.innerHTML = "";
+            this.mobMenuHeader(text);
+
+            setDrop.style = "display:none;"
+            this.setOn = 1 - this.setOn;
+        })
+
+        document.getElementById("theme_select_dark").addEventListener("click", () =>{
+            document.getElementById("color_theme").setAttribute("href", "css/styles.css");
+            document.getElementById("cogs").setAttribute("src", "img/cogs_white.svg");
+            document.getElementById("hamb").setAttribute("src", "img/hamb_white.svg");
+
+            setDrop.style = "display:none;"
+            this.setOn = 1 - this.setOn;
+        })
+        document.getElementById("theme_select_light").addEventListener("click", () =>{
+            document.getElementById("color_theme").setAttribute("href", "css/styles-light.css");
+            document.getElementById("cogs").setAttribute("src", "img/cogs_dark.svg");
+            document.getElementById("hamb").setAttribute("src", "img/hamb_dark.svg");
+
+            setDrop.style = "display:none;"
+            this.setOn = 1 - this.setOn;
+        })
     }
 
     createMenu(){
@@ -20,17 +99,10 @@ class Interface{
         about.className = "menu_btn";
         about.innerHTML = "<span class='lang_opt' id='desk_about'>[Om mig]</span>";
 
-        let aboutContent = document.createElement("p");
-        aboutContent.className = "menu_content";
-        aboutContent.innerHTML = "<span class='lang_opt' id='tmp_about'>Jag heter Daniel.</span>";
-
-        about.addEventListener("click", function(){
-            if (aboutContent.style.display === "none"){
-                aboutContent.style.display = "block";
-            }
-            else{
-                aboutContent.style.display = "none";
-            }
+        about.addEventListener("click", () =>{
+            document.getElementById("main_about").style = "display: block;"
+            document.getElementById("main_works").style = "display: none;"
+            document.getElementById("main_contact").style = "display: none;"
         })
 
         //----------"Selected works"----------//
@@ -40,6 +112,14 @@ class Interface{
         works.className = "menu_btn";
         works.innerHTML = "<span class='lang_opt' id='desk_works'>[Utvalda arbeten]</span>";
 
+        works.addEventListener("click", () =>{
+            document.getElementById("main_about").style = "display: none;"
+            document.getElementById("main_works").style = "display: block;"
+            document.getElementById("main_contact").style = "display: none;"
+
+            this.slideShow();
+        })
+
         //----------"Contact"----------//
 
         let contact = document.createElement("button");
@@ -47,10 +127,15 @@ class Interface{
         contact.className = "menu_btn";
         contact.innerHTML = "<span class='lang_opt' id='desk_contact'>[Kontakt]</span>";
 
+        contact.addEventListener("click", () =>{
+            document.getElementById("main_about").style = "display: none;"
+            document.getElementById("main_works").style = "display: none;"
+            document.getElementById("main_contact").style = "display: block;"
+        })
+
         //----------------------------//
         
         menuDiv.appendChild(about);
-        menuDiv.appendChild(aboutContent);
 
         menuDiv.appendChild(works);
 
@@ -63,6 +148,67 @@ class Interface{
         
         let underTitle = new TextRunner(text, "under_title");
         underTitle.runner();
+    }
+
+    createMenuMobile(){
+        let underTitle = document.getElementById("under_title");
+        let menuDrop = document.getElementById("menu_drop");
+
+        document.getElementById("mob_menu_btn").addEventListener("click", () =>{
+            if (this.menuOn === 0){
+                menuDrop.style = "display: flex; flex-direction: column; justify-content: space-between;"
+            }
+            else{
+                menuDrop.style = "display:none;"
+            }
+            this.menuOn = 1 - this.menuOn;
+
+            document.getElementById("set_drop").style = "display:none;"        
+        })
+
+        let mobAbout = document.getElementById("mob_about_btn");
+        mobAbout.addEventListener("click", () =>{
+            underTitle.innerHTML = "";
+            let text = "└── " + document.getElementById("mob_about").innerHTML;
+            this.mobMenuHeader(text);
+
+            document.getElementById("main_about").style = "display: block;"
+            document.getElementById("main_works").style = "display: none;"
+            document.getElementById("main_contact").style = "display: none;"
+
+            menuDrop.style = "display:none;"
+            this.menuOn = 1 - this.menuOn;
+        })
+
+        let mobWorks = document.getElementById("mob_works_btn");
+        mobWorks.addEventListener("click", () =>{
+            underTitle.innerHTML = "";
+            let text = "└── " + document.getElementById("mob_works").innerHTML;
+            this.mobMenuHeader(text);
+
+            document.getElementById("main_about").style = "display: none;"
+            document.getElementById("main_works").style = "display: block;"
+            document.getElementById("main_contact").style = "display: none;"
+
+            this.slideShow();
+
+            menuDrop.style = "display:none;"
+            this.menuOn = 1 - this.menuOn;
+        })
+
+        let mobContact = document.getElementById("mob_contact_btn");
+        mobContact.addEventListener("click", () =>{
+            underTitle.innerHTML = "";
+            let text = "└── " + document.getElementById("mob_contact").innerHTML;
+            this.mobMenuHeader(text);
+
+            document.getElementById("main_about").style = "display: none;"
+            document.getElementById("main_works").style = "display: none;"
+            document.getElementById("main_contact").style = "display: block;"
+
+            menuDrop.style = "display:none;"
+            this.menuOn = 1 - this.menuOn;
+        })
     }
 
     slideShow(){
@@ -85,7 +231,9 @@ class Interface{
         slideItems[i].style = "display:grid;";
         dots[i].className = "dot_focus";
 
-        prev.addEventListener("click", function(){
+        console.log("1: " + i);
+
+        prev.addEventListener("click", () =>{
             if (i != 0){
                 slideItems[i].style = "display:none;";
                 dots[i].className = "dot";
@@ -99,9 +247,10 @@ class Interface{
             
             slideItems[i].style = "display:grid;";
             dots[i].className = "dot_focus";
+            console.log("prev: " + i);
         });
 
-        next.addEventListener("click", function(){
+        next.addEventListener("click", () =>{
             if (i != slideItems.length - 1){
                 slideItems[i].style = "display:none;";
                 dots[i].className = "dot";
@@ -115,8 +264,7 @@ class Interface{
 
             slideItems[i].style = "display:grid;";
             dots[i].className = "dot_focus";
-        })
-
-        
+            console.log("next: " + i);
+        });        
     }
 }
